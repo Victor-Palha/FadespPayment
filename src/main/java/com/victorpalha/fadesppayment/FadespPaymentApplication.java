8abd3ac6cf286a054fb82fe0fc9384030e2f00b4
strahd <jv.palha@gmail.com>
2025-06-06 19:49:58 -0300

    add: start project template

---
+++ src/main/java/com/victorpalha/fadesppayment/FadespPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.victorpalha.fadesppayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FadespPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FadespPaymentApplication.class, args);
+    }
+
+}
